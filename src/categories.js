9a15792a15537fef66d9383d038189de160634b5
Harshankrishna <harshankrishdev003@gmail.com>
2023-07-27 16:59:01 +0530

    first commit

---
+++ src/categories.js
@@ -0,0 +1,21 @@
+import watch from "./assets/iwatch.jpg";
+import phone from "./assets/1.jpg";
+import mac from "./assets/imac.jpg";
+
+const categories = [
+  {
+    name: "Veg",
+    img: `${watch}`,
+  },
+  {
+    name: "Non-Veg",
+    img: `${phone}`,
+  },
+
+  {
+    name: "Desert",
+    img: `${mac}`,
+  },
+];
+
+export default categories;
